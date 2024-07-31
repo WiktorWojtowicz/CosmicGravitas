eda2a673ffef1fd17cc8c7224ad97d67bc2f59c2
GameShoot8050 <Wiktorwojt@interia.pl>
2024-07-31 15:17:52 +0200

    ujoisgriorjsgiojsrgjiosrg

---
+++ build/CMakeFiles/CosmicGravitas.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CosmicGravitas.dir/src/Clock.cpp.obj"
+  "CMakeFiles/CosmicGravitas.dir/src/Clock.cpp.obj.d"
+  "CMakeFiles/CosmicGravitas.dir/src/main.cpp.obj"
+  "CMakeFiles/CosmicGravitas.dir/src/main.cpp.obj.d"
+  "CosmicGravitas.exe"
+  "CosmicGravitas.exe.manifest"
+  "CosmicGravitas.pdb"
+  "libCosmicGravitas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CosmicGravitas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CosmicGravitas.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CosmicGravitas.
+# This may be replaced when dependencies are built.
