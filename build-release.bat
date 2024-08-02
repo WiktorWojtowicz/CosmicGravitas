6797b2a8deca4c08dcf997acf66aef00eaada8ba
GameShoot8050 <Wiktorwojt@interia.pl>
2024-08-02 19:32:54 +0200

    Some minor improvements

---
+++ build-release.bat
@@ -0,0 +1,33 @@
+@echo off
+
+set ROOT_DIR=%cd%
+set BUILD_DIR="%ROOT_DIR%\build"
+set RELEASE_DIR="%BUILD_DIR%\release"
+
+mkdir %BUILD_DIR% > nul 2>&1
+mkdir %RELEASE_DIR% > nul 2>&1
+
+cd %RELEASE_DIR%
+
+echo Running CMake
+cmake -S %ROOT_DIR% -DCMAKE_BUILD_TYPE="Release" -B %RELEASE_DIR%
+if %ERRORLEVEL% NEQ 0 (
+    echo CMake failed with exit code %ERRORLEVEL%.
+    exit
+)
+
+echo Building...
+cmake --build %RELEASE_DIR% --parallel
+if %ERRORLEVEL% NEQ 0 (
+    echo Build failed with exit code %ERRORLEVEL%.
+    exit
+)
+
+echo Running CPack
+cpack
+if %ERRORLEVEL% NEQ 0 (
+    echo CPack failed with exit code %ERRORLEVEL%.
+    exit
+)
+
+echo Done! your executable is in %RELEASE_DIR%
---
+++ build-release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+ROOT_DIR=$(pwd)
+BUILD_DIR="$ROOT_DIR/build"
+RELEASE_DIR="$BUILD_DIR/release"
+
+[ ! -d "$BUILD_DIR" ] && mkdir -p "$BUILD_DIR"
+[ ! -d "$RELEASE_DIR" ] && mkdir -p "$RELEASE_DIR"
+
+cd "$RELEASE_DIR"
+
+echo "Running CMake"
+cmake -S "$ROOT_DIR" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE="Release" -B "$RELEASE_DIR"
+
+echo "Building..."
+cmake --build "$RELEASE_DIR" --parallel
+
+
+echo "Running CPack"
+cpack
